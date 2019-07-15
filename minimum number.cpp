c57d786ebe57456a7eea161664598fdc2a5cfe77
AnalaVenkataSaiAmrutha <42970922+AnalaVenkataSaiAmrutha@users.noreply.github.com>
2019-07-15 15:58:47 +0530

    Add files via upload

---
+++ minimum number.cpp
@@ -0,0 +1,25 @@
+
+/*Minimum element of an array */
+#include<stdio.h>
+int main()
+{
+	int a[50],i,n,min;
+	printf("Ener the size of array :\n");
+	scanf("%d",&n);
+	printf("Enter the array elements:\n");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+		
+	}
+	min=a[0];
+	for(i=1;i<n;i++)
+	{
+		if(a[i]<min)
+		min=a[i];
+		
+	}
+	printf("Minimum number is %d ",min);
+	return 0;
+	
+}
