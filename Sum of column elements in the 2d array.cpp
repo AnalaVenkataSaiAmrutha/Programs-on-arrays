85ce385e3a534168e934e787dc410be69315da44
AnalaVenkataSaiAmrutha <42970922+AnalaVenkataSaiAmrutha@users.noreply.github.com>
2019-07-23 19:35:42 +0530

    Add files via upload

---
+++ Sum of column elements in the 2d array.cpp
@@ -0,0 +1,35 @@
+/*Sum of row elements in 2d array */
+#include<stdio.h>
+int main()
+{
+	int a[20][20],i,j,r,c,sum=0;
+	printf("Enter the rows and cols:\n");
+	scanf("%d%d",&r,&c);
+	printf("Enter matrix elements:\n");
+	for(i=0;i<r;i++)
+	{
+		for(j=0;j<c;j++)
+		  scanf("%d",&a[i][j]);
+	}
+	printf("Array elements are:\n");
+	for(i=0;i<r;i++)
+	{
+		for(j=0;j<c;j++)
+		  printf("%d",a[i][j]);
+	
+	  printf("\n");
+}
+	for(i=0;i<r;i++)
+	{
+	    sum=0;
+		for(j=0;j<c;j++)
+		{
+		
+		  sum=sum+a[j][i];
+        
+        }
+	     printf("Column %d Sum:%d\n",i,sum);
+    
+    }
+	return 0;
+}
