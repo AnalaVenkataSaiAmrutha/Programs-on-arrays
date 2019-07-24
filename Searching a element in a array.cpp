deb27a23a57b04107bffc2fb00980fa0b42faf5b
AnalaVenkataSaiAmrutha <42970922+AnalaVenkataSaiAmrutha@users.noreply.github.com>
2019-07-24 16:17:50 +0530

    Add files via upload

---
+++ Searching a element in a array.cpp
@@ -0,0 +1,28 @@
+/*Searching the given element in the array */
+#include<stdio.h>
+int main()
+{
+	int a[10],n,i,s,cnt=0;
+	printf("Enter the size of array\n");
+	scanf("%d",&n);
+	printf("Enter the array elements\n");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	printf("Enter the element to be searched:\n");
+	scanf("%d",&s);
+	for(i=0;i<n;i++)
+	{
+		if(a[i]==s)
+		{
+			cnt=1;
+			break;
+		}
+	}
+	if(cnt==1)
+	  printf("%d is found in the array\n",s);
+	else
+	  printf("%d is not found in the array\n",s);
+	return 0;
+}
