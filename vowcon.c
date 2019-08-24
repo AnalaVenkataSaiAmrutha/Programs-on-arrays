44d9238c7a5ffbfe1d2ea8200f31e8d13a066383
AnalaVenkataSaiAmrutha <42970922+AnalaVenkataSaiAmrutha@users.noreply.github.com>
2019-08-24 21:13:36 +0530

    Add files via upload

---
+++ vowcon.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	char s1[10];
+	int i,cnt1=0,cnt2=0;
+	gets(s1);
+	for(i=0;s1[i]!='\0';i++)
+	{
+		if(s1[i]=='a'||s1[i]=='e'||s1[i]=='i'||s1[i]=='o'||s1[i]=='u'||s1[i]=='A'||s1[i]=='E'||s1[i]=='I'||s1[i]=='O'||s1[i]=='U')
+		cnt1++;
+		else
+		cnt2++;
+	}
+	printf("Vowels:%d Consonants:%d",cnt1,cnt2);
+	return 0;
+}
