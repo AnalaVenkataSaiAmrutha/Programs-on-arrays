6c0b5bdf58062bc8b808c0d69d4ff4f87f1a6bca
AnalaVenkataSaiAmrutha <42970922+AnalaVenkataSaiAmrutha@users.noreply.github.com>
2019-09-21 11:02:02 +0530

    Add files via upload

---
+++ revarrptr.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int *arr,n,i,*ptr;
+	printf("Enter array size:");
+	scanf("%d",&n);
+	arr=(int*)malloc(n*sizeof(int));
+	printf("Enter array elements");
+	for(i=0;i<n;i++)
+	scanf("%d",(arr+i));
+	ptr=arr;
+	printf("Reversed array elements are");
+	for(i=n-1;i>=0;i--)
+	{
+	   printf("%d ",*(ptr+i));
+	  	
+	}
+	
+	
+    	
+	return 0;
+}
+
